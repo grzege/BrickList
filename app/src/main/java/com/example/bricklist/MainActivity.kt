898445d53bbfbc441f19499f5d36efb2c1c4fb30
Grzegorz Otworowski <grzegorz.otworowski@student.put.poznan.pl>
2020-05-18 17:44:34 +0200

    Main_activity_layout
    Added main activity layout without functionality.

--- app/src/main/java/com/example/bricklist/MainActivity.kt
+++ app/src/main/java/com/example/bricklist/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.bricklist
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.main_listview)
+        val listItems = arrayListOf<String>("alfa","beta","gamma")
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,listItems)
+        listView.adapter=adapter
     }
 }
